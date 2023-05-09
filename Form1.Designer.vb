03037c291118cf96a110883b87f222f08a60371e
rasikapatil3499 <66173415+rasikapatil3499@users.noreply.github.com>
2023-05-09 16:03:29 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,336 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.seven = New System.Windows.Forms.Button()
+        Me.eight = New System.Windows.Forms.Button()
+        Me.nine = New System.Windows.Forms.Button()
+        Me.addition = New System.Windows.Forms.Button()
+        Me.subtraction = New System.Windows.Forms.Button()
+        Me.six = New System.Windows.Forms.Button()
+        Me.five = New System.Windows.Forms.Button()
+        Me.four = New System.Windows.Forms.Button()
+        Me.multiplication = New System.Windows.Forms.Button()
+        Me.three = New System.Windows.Forms.Button()
+        Me.two = New System.Windows.Forms.Button()
+        Me.one = New System.Windows.Forms.Button()
+        Me.divide = New System.Windows.Forms.Button()
+        Me.equal = New System.Windows.Forms.Button()
+        Me.dot = New System.Windows.Forms.Button()
+        Me.zero = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'seven
+        '
+        Me.seven.BackColor = System.Drawing.Color.White
+        Me.seven.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.seven.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.seven.Location = New System.Drawing.Point(30, 81)
+        Me.seven.Name = "seven"
+        Me.seven.Size = New System.Drawing.Size(59, 38)
+        Me.seven.TabIndex = 0
+        Me.seven.Text = "7"
+        Me.seven.UseVisualStyleBackColor = False
+        '
+        'eight
+        '
+        Me.eight.BackColor = System.Drawing.Color.White
+        Me.eight.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.eight.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.eight.Location = New System.Drawing.Point(112, 81)
+        Me.eight.Name = "eight"
+        Me.eight.Size = New System.Drawing.Size(59, 38)
+        Me.eight.TabIndex = 1
+        Me.eight.Text = "8"
+        Me.eight.UseVisualStyleBackColor = False
+        '
+        'nine
+        '
+        Me.nine.BackColor = System.Drawing.Color.White
+        Me.nine.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nine.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.nine.Location = New System.Drawing.Point(192, 81)
+        Me.nine.Name = "nine"
+        Me.nine.Size = New System.Drawing.Size(59, 38)
+        Me.nine.TabIndex = 2
+        Me.nine.Text = "9"
+        Me.nine.UseVisualStyleBackColor = False
+        '
+        'addition
+        '
+        Me.addition.BackColor = System.Drawing.Color.White
+        Me.addition.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addition.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.addition.Location = New System.Drawing.Point(257, 81)
+        Me.addition.Name = "addition"
+        Me.addition.Size = New System.Drawing.Size(59, 38)
+        Me.addition.TabIndex = 3
+        Me.addition.Text = "+"
+        Me.addition.UseVisualStyleBackColor = False
+        '
+        'subtraction
+        '
+        Me.subtraction.BackColor = System.Drawing.Color.White
+        Me.subtraction.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.subtraction.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.subtraction.Location = New System.Drawing.Point(257, 139)
+        Me.subtraction.Name = "subtraction"
+        Me.subtraction.Size = New System.Drawing.Size(59, 38)
+        Me.subtraction.TabIndex = 7
+        Me.subtraction.Text = "-"
+        Me.subtraction.UseVisualStyleBackColor = False
+        '
+        'six
+        '
+        Me.six.BackColor = System.Drawing.Color.White
+        Me.six.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.six.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.six.Location = New System.Drawing.Point(192, 139)
+        Me.six.Name = "six"
+        Me.six.Size = New System.Drawing.Size(59, 38)
+        Me.six.TabIndex = 6
+        Me.six.Text = "6"
+        Me.six.UseVisualStyleBackColor = False
+        '
+        'five
+        '
+        Me.five.BackColor = System.Drawing.Color.White
+        Me.five.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.five.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.five.Location = New System.Drawing.Point(112, 139)
+        Me.five.Name = "five"
+        Me.five.Size = New System.Drawing.Size(59, 38)
+        Me.five.TabIndex = 5
+        Me.five.Text = "5"
+        Me.five.UseVisualStyleBackColor = False
+        '
+        'four
+        '
+        Me.four.BackColor = System.Drawing.Color.White
+        Me.four.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.four.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.four.Location = New System.Drawing.Point(30, 139)
+        Me.four.Name = "four"
+        Me.four.Size = New System.Drawing.Size(59, 38)
+        Me.four.TabIndex = 4
+        Me.four.Text = "4"
+        Me.four.UseVisualStyleBackColor = False
+        '
+        'multiplication
+        '
+        Me.multiplication.BackColor = System.Drawing.Color.White
+        Me.multiplication.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.multiplication.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.multiplication.Location = New System.Drawing.Point(257, 201)
+        Me.multiplication.Name = "multiplication"
+        Me.multiplication.Size = New System.Drawing.Size(59, 38)
+        Me.multiplication.TabIndex = 11
+        Me.multiplication.Text = "*"
+        Me.multiplication.UseVisualStyleBackColor = False
+        '
+        'three
+        '
+        Me.three.BackColor = System.Drawing.Color.White
+        Me.three.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.three.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.three.Location = New System.Drawing.Point(192, 201)
+        Me.three.Name = "three"
+        Me.three.Size = New System.Drawing.Size(59, 38)
+        Me.three.TabIndex = 10
+        Me.three.Text = "3"
+        Me.three.UseVisualStyleBackColor = False
+        '
+        'two
+        '
+        Me.two.BackColor = System.Drawing.Color.White
+        Me.two.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.two.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.two.Location = New System.Drawing.Point(112, 201)
+        Me.two.Name = "two"
+        Me.two.Size = New System.Drawing.Size(59, 38)
+        Me.two.TabIndex = 9
+        Me.two.Text = "2"
+        Me.two.UseVisualStyleBackColor = False
+        '
+        'one
+        '
+        Me.one.BackColor = System.Drawing.Color.White
+        Me.one.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.one.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.one.Location = New System.Drawing.Point(30, 201)
+        Me.one.Name = "one"
+        Me.one.Size = New System.Drawing.Size(59, 38)
+        Me.one.TabIndex = 8
+        Me.one.Text = "1"
+        Me.one.UseVisualStyleBackColor = False
+        '
+        'divide
+        '
+        Me.divide.BackColor = System.Drawing.Color.White
+        Me.divide.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.divide.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.divide.Location = New System.Drawing.Point(257, 265)
+        Me.divide.Name = "divide"
+        Me.divide.Size = New System.Drawing.Size(59, 38)
+        Me.divide.TabIndex = 15
+        Me.divide.Text = "/"
+        Me.divide.UseVisualStyleBackColor = False
+        '
+        'equal
+        '
+        Me.equal.BackColor = System.Drawing.Color.White
+        Me.equal.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.equal.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.equal.Location = New System.Drawing.Point(192, 265)
+        Me.equal.Name = "equal"
+        Me.equal.Size = New System.Drawing.Size(59, 38)
+        Me.equal.TabIndex = 14
+        Me.equal.Text = "="
+        Me.equal.UseVisualStyleBackColor = False
+        '
+        'dot
+        '
+        Me.dot.BackColor = System.Drawing.Color.White
+        Me.dot.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dot.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.dot.Location = New System.Drawing.Point(112, 265)
+        Me.dot.Name = "dot"
+        Me.dot.Size = New System.Drawing.Size(59, 38)
+        Me.dot.TabIndex = 13
+        Me.dot.Text = "."
+        Me.dot.UseVisualStyleBackColor = False
+        '
+        'zero
+        '
+        Me.zero.BackColor = System.Drawing.Color.White
+        Me.zero.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.zero.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.zero.Location = New System.Drawing.Point(30, 265)
+        Me.zero.Name = "zero"
+        Me.zero.Size = New System.Drawing.Size(59, 38)
+        Me.zero.TabIndex = 12
+        Me.zero.Text = "0"
+        Me.zero.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.TextBox1.Location = New System.Drawing.Point(30, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(221, 38)
+        Me.TextBox1.TabIndex = 16
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.clear)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.equal)
+        Me.Panel1.Controls.Add(Me.divide)
+        Me.Panel1.Controls.Add(Me.seven)
+        Me.Panel1.Controls.Add(Me.eight)
+        Me.Panel1.Controls.Add(Me.dot)
+        Me.Panel1.Controls.Add(Me.nine)
+        Me.Panel1.Controls.Add(Me.zero)
+        Me.Panel1.Controls.Add(Me.addition)
+        Me.Panel1.Controls.Add(Me.multiplication)
+        Me.Panel1.Controls.Add(Me.four)
+        Me.Panel1.Controls.Add(Me.three)
+        Me.Panel1.Controls.Add(Me.five)
+        Me.Panel1.Controls.Add(Me.two)
+        Me.Panel1.Controls.Add(Me.six)
+        Me.Panel1.Controls.Add(Me.one)
+        Me.Panel1.Controls.Add(Me.subtraction)
+        Me.Panel1.Location = New System.Drawing.Point(12, 16)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(345, 321)
+        Me.Panel1.TabIndex = 17
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.Panel1)
+        Me.Panel2.Location = New System.Drawing.Point(295, 27)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(381, 357)
+        Me.Panel2.TabIndex = 18
+        '
+        'clear
+        '
+        Me.clear.BackColor = System.Drawing.Color.White
+        Me.clear.Font = New System.Drawing.Font("Microsoft YaHei UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.clear.Location = New System.Drawing.Point(257, 21)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(59, 38)
+        Me.clear.TabIndex = 17
+        Me.clear.Text = "c"
+        Me.clear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(783, 451)
+        Me.Controls.Add(Me.Panel2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents seven As System.Windows.Forms.Button
+    Friend WithEvents eight As System.Windows.Forms.Button
+    Friend WithEvents nine As System.Windows.Forms.Button
+    Friend WithEvents addition As System.Windows.Forms.Button
+    Friend WithEvents subtraction As System.Windows.Forms.Button
+    Friend WithEvents six As System.Windows.Forms.Button
+    Friend WithEvents five As System.Windows.Forms.Button
+    Friend WithEvents four As System.Windows.Forms.Button
+    Friend WithEvents multiplication As System.Windows.Forms.Button
+    Friend WithEvents three As System.Windows.Forms.Button
+    Friend WithEvents two As System.Windows.Forms.Button
+    Friend WithEvents one As System.Windows.Forms.Button
+    Friend WithEvents divide As System.Windows.Forms.Button
+    Friend WithEvents equal As System.Windows.Forms.Button
+    Friend WithEvents dot As System.Windows.Forms.Button
+    Friend WithEvents zero As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents clear As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,152 @@
+﻿Public Class Form1
+    Dim fnum As Decimal
+    Dim snum As Decimal
+    Dim opr As Integer
+    Dim opr_selector As Boolean = False
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+ 
+    Private Sub zero_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zero.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "0"
+        Else
+            TextBox1.Text = "0"
+        End If
+    End Sub
+
+    Private Sub one_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles one.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "1"
+        Else
+            TextBox1.Text = "1"
+        End If
+    End Sub
+
+    Private Sub four_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles four.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "4"
+        Else
+            TextBox1.Text = "4"
+        End If
+    End Sub
+
+    Private Sub three_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles three.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "3"
+        Else
+            TextBox1.Text = "3"
+        End If
+    End Sub
+
+    Private Sub two_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles two.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "2"
+        Else
+            TextBox1.Text = "2"
+        End If
+    End Sub
+
+    Private Sub five_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles five.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "5"
+        Else
+            TextBox1.Text = "5"
+        End If
+    End Sub
+
+    Private Sub six_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles six.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "6"
+        Else
+            TextBox1.Text = "6"
+        End If
+    End Sub
+
+    Private Sub seven_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles seven.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "7"
+        Else
+            TextBox1.Text = "7"
+        End If
+    End Sub
+
+    Private Sub eight_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles eight.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "8"
+        Else
+            TextBox1.Text = "8"
+        End If
+    End Sub
+
+    Private Sub nine_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nine.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "9"
+        Else
+            TextBox1.Text = "9"
+        End If
+    End Sub
+
+    Private Sub addition_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles addition.Click
+        fnum = TextBox1.Text
+        TextBox1.Text = 0
+        opr_selector = True
+        opr = 1 ' = +
+    End Sub
+
+    Private Sub subtraction_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles subtraction.Click
+        fnum = TextBox1.Text
+        TextBox1.Text = 0
+        opr_selector = True
+        opr = 2 ' = -
+    End Sub
+
+    Private Sub multiplication_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles multiplication.Click
+        fnum = TextBox1.Text
+        TextBox1.Text = 0
+        opr_selector = True
+        opr = 3 ' = *
+    End Sub
+
+    Private Sub divide_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles divide.Click
+        fnum = TextBox1.Text
+        TextBox1.Text = 0
+        opr_selector = True
+        opr = 4 ' = /
+    End Sub
+
+    Private Sub equal_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles equal.Click
+        If opr_selector = True Then
+            snum = TextBox1.Text
+            If opr = 1 Then
+                TextBox1.Text = fnum + snum
+            ElseIf opr = 2 Then
+                TextBox1.Text = fnum - snum
+            ElseIf opr = 3 Then
+                TextBox1.Text = fnum * snum
+            Else
+                If snum = 0 Then
+                    TextBox1.Text = "error!"
+                Else
+                    TextBox1.Text = fnum / snum
+            End If
+        End If
+            opr_selector = False
+        End If
+    End Sub
+
+    Private Sub dot_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dot.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "."
+        Else
+            TextBox1.Text = "."
+        End If
+    End Sub
+
+    Private Sub clear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles clear.Click
+        TextBox1.Text=""
+    End Sub
+End Class
